d27041e6f60c21087f851782fb0e51daaa48eeb5
rajeshycode <136836225+rajeshycode@users.noreply.github.com>
2023-10-22 19:12:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 window.promises = [];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+// Function to create a promise with a random delay between 1 and 5 seconds
+function createRandomPromise() {
+  const delay = Math.floor(Math.random() * 5) + 1; // Random delay between 1 and 5 seconds
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`Promise resolved after ${delay} seconds`);
+    }, delay * 1000);
+  });
+}
+
+// Create and push five random promises to the `promises` array
+for (let i = 0; i < 5; i++) {
+  window.promises.push(createRandomPromise());
+}
+
+// Use Promise.any to wait for the first promise to resolve
+Promise.any(window.promises)
+  .then((result) => {
+    // Print the result to the div with the id 'output'
+    document.getElementById('output').innerText = result;
+  })
+  .catch((error) => {
+    console.error("An error occurred:", error);
+  });
